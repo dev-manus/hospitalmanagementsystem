f4fc1b83d38f0fafd34be27e8dad7b5244e85fe7
Tushar <tsbauskar.work@gmail.com>
2021-10-23 16:15:22 +0530

    Fixes and few changes

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'hospital_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'hospital-management-system',
-#         'USER': 'postgres',
-#         'PASSWORD': '1208',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hospital-management-system',
+        'USER': 'postgres',
+        'PASSWORD': '1208',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
 
 
 # Password validation
--- staff/views.py
+++ staff/views.py
@@ -1,13 +1,17 @@
-from django.http.response import HttpResponseRedirect
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required, user_passes_test
 from doctor.models import Doctor
-from patient.models import Patient
+from patient.models import Appointment, Patient
 from .forms import StaffRegistrationForm
 from django.contrib.auth.models import User
 from django.urls import reverse
-# Create your views here.
+
+
+def is_admin(user):
+    return user.groups.filter(name='ADMIN').exists()
 
 
 def staff_register(request):
@@ -28,20 +32,14 @@ def staff_register(request):
     return render(request, 'staff/register.html', context)
 
 
-class StaffLoginView(LoginView):
-    template_name = 'staff/login.html'
-    redirect_authenticated_user = True
-
-    def get_success_url(self):
-        return reverse('staff-dashboard')
-
-
+@login_required(login_url='/staff/login')
+@user_passes_test(is_admin, login_url='/staff/login')
 def staff_dashboard(request):
     doctors = Doctor.objects.all().order_by('-id')
     patients = Patient.objects.all().order_by('-id')
     doctorcount = Doctor.objects.all().filter(status=True).count()
     pendingdoctorcount = Doctor.objects.all().filter(status=False).count()
-
+    appointmentscount = Appointment.objects.all().filter(status=False).count()
     patientcount = Patient.objects.all().filter(status=True).count()
     pendingpatientcount = Patient.objects.all().filter(status=False).count()
 
@@ -52,5 +50,6 @@ def staff_dashboard(request):
         'pendingdoctorcount': pendingdoctorcount,
         'patientcount': patientcount,
         'pendingpatientcount': pendingpatientcount,
+        'apppointmentscount': appointmentscount
     }
     return render(request, 'staff/dashboard.html', context)
