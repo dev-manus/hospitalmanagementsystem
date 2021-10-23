b59d8b81313f76f6794921d0ae1cde21101b2a66
utsavk28 <utsavkhatu416@gmail.com>
2021-10-23 15:09:56 +0530

    Minor Changes

--- staff/urls.py
+++ staff/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.staff_dashboard, name='staff-dashboard'),
+    path('dashboard/', views.staff_dashboard, name='staff-dashboard'),
     path('register/', views.staff_register, name='staff-register'),
     path('login/', views.StaffLoginView.as_view(),name='staff-login'),
     
